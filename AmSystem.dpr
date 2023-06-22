afb45283cb6d1cadde46ad08b96e02c68374446e
Mayke Santos <86814900+Mayke9873@users.noreply.github.com>
2023-06-21 23:18:20 -0300

    Adicionado fornecedor ao projeto.

--- AmSystem.dpr
+++ AmSystem.dpr
@@ -20,7 +20,8 @@ uses
   uVenda in 'src\uVenda.pas',
   uProduto in 'src\uProduto.pas',
   uFuncionario in 'src\uFuncionario.pas',
-  OKCANCL1 in 'c:\program files (x86)\embarcadero\studio\22.0\ObjRepos\EN\DelphiWin32\OKCANCL1.PAS' {OKBottomDlg};
+  FrmFornecedor in 'src\view\FrmFornecedor.pas' {fFornecedor},
+  Consts in 'Consts.pas';
 
 {$R *.res}
 
