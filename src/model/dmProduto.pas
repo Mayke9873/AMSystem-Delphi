e01406476013a6dd3058aabc8f9bef890842fe89
Mayke Santos <86814900+Mayke9873@users.noreply.github.com>
2023-07-03 22:31:02 -0300

    Adicionado DM para operacoes de produtos.

---
+++ src/model/dmProduto.pas
@@ -0,0 +1,50 @@
+unit dmProduto;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, uDM, Data.DB, ZSqlUpdate, ZAbstractRODataset, ZAbstractDataset, ZDataset,
+  ZAbstractConnection, ZConnection;
+
+type
+  TdmProdutos = class(TDM)
+    qProduto: TZQuery;
+    qProdutoid: TIntegerField;
+    qProdutodescricao: TWideStringField;
+    qProdutoestoque: TFloatField;
+    qProdutounidade: TWideStringField;
+    qProdutopCompra: TFloatField;
+    qProdutopLucro: TFloatField;
+    qProdutopVenda: TFloatField;
+    qProdutoIdGrupo: TIntegerField;
+    qProdutogrupo: TWideStringField;
+    qProdutodtRegistro: TDateField;
+    qProdutoativo: TWideStringField;
+    dProduto: TDataSource;
+    uProduto: TZUpdateSQL;
+    qMovEstoque: TZQuery;
+    qMovEstoqueid: TIntegerField;
+    qMovEstoqueidproduto: TIntegerField;
+    qMovEstoquequantidade: TFloatField;
+    qMovEstoquedataMov: TDateField;
+    qMovEstoqueidUsuario: TSmallintField;
+    qMovEstoqueidFornecedor: TSmallintField;
+    qMovEstoquetipoMov: TWideStringField;
+    qMovEstoqueobs: TWideStringField;
+    uMovEstoque: TZUpdateSQL;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  dmProdutos: TdmProdutos;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
