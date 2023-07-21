39a1b5311ce3dc8b64252e03459ad8b9335acd1c
Mayke Santos <86814900+Mayke9873@users.noreply.github.com>
2023-07-20 22:54:48 -0300

    Realocado DM principal.

--- AmSystem.dpr
+++ AmSystem.dpr
@@ -2,7 +2,7 @@ program AmSystem;
 
 uses
   Vcl.Forms,
-  uDM in 'src\view\uDM.pas' {DM: TDataModule},
+  uDM in 'src\model\uDM.pas' {DM: TDataModule},
   FrmPrincipal in 'src\view\FrmPrincipal.pas' {fPrincipal},
   FrmVenda in 'src\view\FrmVenda.pas' {fVenda},
   FrmConsultaVenda in 'src\view\FrmConsultaVenda.pas' {fConsultaVendas},
