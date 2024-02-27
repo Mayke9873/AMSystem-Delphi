e575ec853c6f8c6d1cf2fa3a7059ddef9009e27f
Mayke Santos <86814900+Mayke9873@users.noreply.github.com>
2024-02-26 22:51:08 -0300

    add property descricao para graver itens da venda.

--- src/uVenda_Itens.pas
+++ src/uVenda_Itens.pas
@@ -12,6 +12,7 @@ type
     FidVenda: Integer;
     Fquantidade: Double;
     Fvalor_unit: Real;
+    Fdescricao: string;
 
     procedure Setdesconto(const Value: Real);
     procedure Setid(const Value: Integer);
@@ -20,6 +21,7 @@ type
     procedure Setquantidade(const Value: Double);
     procedure Settotal(const Value: Real);
     procedure Setvalor_unit(const Value: Real);
+    procedure Setdescricao(const Value: string);
 
   public
     property id: Integer read Fid write Setid;
@@ -29,6 +31,7 @@ type
     property valor_unit: Real read Fvalor_unit write Setvalor_unit;
     property desconto: Real read Fdesconto write Setdesconto;
     property total: Real read Ftotal write Settotal;
+    property descricao: string read Fdescricao write Setdescricao;
   end;
 
 implementation
@@ -38,6 +41,11 @@ implementation
 procedure TVenda_Itens.Setdesconto(const Value: Real);
 begin
   Fdesconto := Value;
+end;
+
+procedure TVenda_Itens.Setdescricao(const Value: string);
+begin
+  Fdescricao := Value;
 end;
 
 procedure TVenda_Itens.Setid(const Value: Integer);
