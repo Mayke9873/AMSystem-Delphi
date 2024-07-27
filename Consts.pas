bdc9b7498752c5dae031c7d6dd14ae261db491f1
Mayke9873 <86814900+Mayke9873@users.noreply.github.com>
2024-07-27 17:06:32 -0300

    Adicionado constantes para trabalhar status

--- Consts.pas
+++ Consts.pas
@@ -5,17 +5,26 @@ interface
 Const
   telaPadrao = 0;
   telaInsert = 1;
-  telaEdit = 2;
+  telaEdit   = 2;
 
 // Tipos movimentacoes de estoque //
-  Ajuste = 'A';
-  Venda = 'V';
-  Compra = 'C';
-  Insercao = 'I';
+  Ajuste    = 'A';
+  Venda     = 'V';
+  Compra    = 'C';
+  Insercao  = 'I';
 
-  Entrada = 1;
-  Saida = -1;
+  Entrada   = 1;
+  Saida     = -1;
 // ------------------------------//
+
+// ----- Status EX ----- //
+  Aberto      = 0;
+  Finalizado  = 1;
+  Excluido    = 2;
+  Cancelado   = 3;
+  Edicao      = 4;
+  Salvo       = 5;
+  Efetivado   = 6;
 
 implementation
 
