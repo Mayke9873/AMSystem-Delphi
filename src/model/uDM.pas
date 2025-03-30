a145c728c9732716c1fbeb9ebe144cf71df5c2a4
Mayke Santos <86814900+Mayke9873@users.noreply.github.com>
2025-03-30 15:34:13 -0300

    Adicionado conexao via arquivo de configuracao.

--- src/model/uDM.pas
+++ src/model/uDM.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Classes, ZAbstractConnection, ZConnection, Data.DB,
   ZAbstractRODataset, ZDataset, ZAbstractDataset, ZSqlUpdate, Vcl.Forms,
-  Winapi.Windows, System.UITypes, ZAbstractTable;
+  Winapi.Windows, System.UITypes, ZAbstractTable, System.IniFiles;
 
 type
   TDM = class(TDataModule)
@@ -21,6 +21,8 @@ type
     procedure qFuncionariodtnascSetText(Sender: TField; const Text: string);
     procedure DataModuleCreate(Sender: TObject);
   private
+    procedure CriaArquivoIni;
+    procedure ConfiguraConexao(AConexao: TZConnection);
     { Private declarations }
   public
     { Public declarations }
@@ -51,17 +53,63 @@ procedure TDM.DataModuleCreate(Sender: TObject);
 var
   I: Integer;
 begin
-  DM.zCon.LibraryLocation := ExtractFilePath(Application.ExeName) + 'libmariadb.dll';
+  for I := 0 to ComponentCount -1 do
+    if Components[I].ClassName = 'TZConnection' then
+    begin
+      ConfiguraConexao(TZConnection(Components[I]));
+      Break;
+    end;
+
   try
     DM.zCon.Connect;
 
     for I := 0 to ComponentCount - 1 do
       if Components[i].ClassName = 'TZTable' then
         (Components[i] as TZTable).Open;
-
   except
-    Application.MessageBox('Nao foi possivel conectar ao banco de dados', 'Atenção', 16);
-    Halt;
+    on E: Exception do
+    begin
+      CriaArquivoIni();
+      Application.MessageBox(PChar('Nao foi possivel conectar ao banco de dados'#13+E.Message + Self.Name), 'Atenção', 16);
+      Halt;
+    end;
+  end;
+end;
+
+procedure TDM.ConfiguraConexao(AConexao: TZConnection);
+var
+  Config: TIniFile;
+begin
+  DM.zCon.LibraryLocation := ExtractFilePath(Application.ExeName) + 'libmariadb.dll';
+
+  Config := TIniFile.Create(ExtractFilePath(Application.ExeName) + 'Config.ini');
+  try
+    AConexao.HostName := Config.ReadString('Conexao', 'hostName', 'localhost');
+    AConexao.Port     := Config.ReadInteger('Conexao', 'port', 3306);
+    AConexao.User     := Config.ReadString('Conexao', 'user', 'root');
+    AConexao.Password := Config.ReadString('Conexao', 'password', '123456');
+    AConexao.Database := Config.ReadString('Conexao', 'database', '');
+  finally
+    Config.Free;
+  end;
+end;
+
+procedure TDM.CriaArquivoIni;
+var
+  Config: TIniFile;
+begin
+  if FileExists(ExtractFilePath(Application.ExeName) + 'Config.ini') then
+    Exit;
+
+  Config := TIniFile.Create(ExtractFilePath(Application.ExeName) + 'Config.ini');
+  try
+    Config.WriteString('Conexao', 'hostName', 'localhost');
+    Config.WriteString('Conexao', 'port', '3306');
+    Config.WriteString('Conexao', 'user', 'root');
+    Config.WriteString('Conexao', 'password', '123456');
+    Config.WriteString('Conexao', 'database', '');
+  finally
+    Config.Free;
   end;
 end;
 
